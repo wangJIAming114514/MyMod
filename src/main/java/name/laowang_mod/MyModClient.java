e2670fd76df13ab0788ecd8ea134126a4b263eb7
Lenovo <win123456789011@outlook.com>
2023-06-30 22:32:54 +0800

    FirstMod

---
+++ src/main/java/name/laowang_mod/MyModClient.java
@@ -0,0 +1,10 @@
+package name.laowang_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
